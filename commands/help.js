e5a2b681e4d8a4645f6f30ec9be235cfcf7447dd
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2021-12-12 13:22:50 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,83 +9,69 @@ module.exports = {
         .setName("category")
         .setDescription("choose the category that you want to see")
         .setRequired(true)
- .addChoice('Economy', 'economy')
+ .addChoice('Moderation', 'moderation')
         .addChoice('Misc', 'misc')
         .addChoice('Fun', 'fun')
-        .addChoice('Economy2', 'economy2')
+        .addChoice('Other', 'other')
     ),
 cooldowns : new Set(),
 	    cooldown : 8,
 	async execute(interaction) {
 const choice = interaction.options.getString('category');
-if (choice === "economy") {
+if (choice === "fun") {
 msg='Help menu for Economy'
 const economyEmbed = new MessageEmbed()
 .setTitle('Economy Commands')
-.setDescription(`:coin: **Economy commands:**
+.setDescription(`:coin: **FUn commands:**
  
-**/transfer** - instead of having to withdraw and give you can give straight from the bank
+**/8ball** - Ask the 8ball any question you want!
 
-**/bal** - Shows your bank balance, wallet balance and overall networth.
+**/angry** - Uh.. Your angry?
 
-**/beg** - Makes you as the user beg and earn a nice little profit!
+**/cry** - Why are you crying? What would make you feel better
 
-**/work** - Makes you work for money because you cant be lazy and expect money!
+**/gayrate** - Check your gay level.
 
-**/deposit** - Deposits the money that you earned into your bank account to keep it safe.
+**/happy** - You feel great, Your happy :)
 
-**/give** - An optional command where you can give some money to a friend or a friend to you.
+**/meme** - Get a meme off reddit and have fun viewing it
 
-**/withdraw** - Can take money out of your bank account and put it in your wallet...
+**/pp** - Check your PP size...
 
-**/rob** - Allows you to rob from people, but why would you want to do that?
-
-**/bet** - Bet cash and make yourself rich!`)
+**/rickroll** - Rickroll your friends lol
+`)
 .setColor('RANDOM')
 return await interaction.reply({embeds: [economyEmbed]})
+
+
 } else if (choice === "misc") {
 msg='Help menu for Misc'
+
 const miscEmbed = new MessageEmbed()
 .setTitle('Misc Commands')
 .setDescription(`
 :wrench: **Misc commands:** 
 
-**/ping** - Check the latency of the bot
+**/bugreport** - Report bugs on our bot
 
-**/invite** - Invite the bot to your server
+**/changelog** - Check our changelog
 
 **/support** - Join the bot support server
 
 **/vote** - Vote for the bot
 
-**/commands** - Check commands for the bot
+**/invite** - Invite the bot to your server
 
-**/commandsinfo** - Check commands status`)
+**/purge** - Purge messages in a channel
+
+**/ping** - Check the bots latency
+
+**/userinfo** - Info about a user
+
+`)
 .setColor('RANDOM')
 return await interaction.reply({embeds: [miscEmbed]})
-} else if (choice === "fun") {
-msg='Help menu for Fun'
-const funEmbed = new MessageEmbed()
-.setTitle('Fun commands')
-.setDescription(`:joy: **Fun commands:** 
-
-**/meme** - Get memes from Reddit
-
-**/gayrate** - Just a funny command to use on friends
-
-**/roast** - Roast others, have some fun 
-
-**/say** - Say something using the bot `)
-.setColor('RANDOM')
-return await interaction.reply({embeds: [funEmbed]})
-} else if (choice === "economy2") {
-msg='Help menu for Economy2'
-const economy2Embed = new MessageEmbed()
-.setTitle('Economy Page 2')
-.setDescription('Coming Soon :tm:')
-.setColor('RANDOM')
-return await interaction.reply({embeds: [economy2Embed]})
-}
+} 
 
 	},
 };
